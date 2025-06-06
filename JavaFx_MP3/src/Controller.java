a6454f1a0cb1ed2d2a1386c6a7b5eb75a9958677
Aini Nurul Amri <118587045+frysushi00@users.noreply.github.com>
2025-06-07 01:10:57 +0700

    1

---
+++ JavaFx_MP3/src/Controller.java
@@ -0,0 +1,141 @@
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Pane header;
+    //@FXML
+    //private ImageView image;
+    @FXML
+    private Button nextButton;
+    @FXML
+    private AnchorPane pane;
+    @FXML
+    private Button pauseButton;
+    @FXML
+    private Button playButton;
+    @FXML
+    private Button previousButton;
+    @FXML
+    private Label songLabel;
+    @FXML
+    private ProgressBar songProgressBar;
+    private Media media;
+    private MediaPlayer mediaPlayer;
+    private File directory;
+    private File[] files;
+    private ArrayList<File> songs;
+    private int songNumber;
+    private Timer timer;
+    private TimerTask task;
+    private boolean running;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        songs = new ArrayList<File>();
+        directory = new File("music");
+        files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                songs.add(file);
+            }
+        }
+        media = new Media(songs.get(songNumber).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        songLabel.setText(songs.get(songNumber).getName());
+        songProgressBar.setStyle("-fx-accent: #B6DD50;");
+    }
+
+    public void playMedia() {
+        beginTimer();
+        mediaPlayer.play();
+    }
+
+    public void pauseMedia() {
+        cancelTimer();
+        mediaPlayer.pause();
+    }
+
+    public void previousMedia() {
+        if (songNumber > 0) {
+            songNumber--;
+            mediaPlayer.stop();
+            if (running) {
+                cancelTimer();
+            }
+            media = new Media(songs.get(songNumber).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+            songLabel.setText(songs.get(songNumber).getName());
+            playMedia();
+        } else {
+            songNumber = songs.size() - 1;
+            mediaPlayer.stop();
+            if (running) {
+                cancelTimer();
+            }
+            media = new Media(songs.get(songNumber).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+            songLabel.setText(songs.get(songNumber).getName());
+            playMedia();
+        }
+    }
+
+    public void nextMedia() {
+        if (songNumber < songs.size() - 1) {
+            songNumber++;
+            mediaPlayer.stop();
+            if (running) {
+                cancelTimer();
+            }
+            media = new Media(songs.get(songNumber).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+            songLabel.setText(songs.get(songNumber).getName());
+            playMedia();
+        } else {
+            songNumber = 0;
+            mediaPlayer.stop();
+            if (running) {
+                cancelTimer();
+            }
+            media = new Media(songs.get(songNumber).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+            songLabel.setText(songs.get(songNumber).getName());
+            playMedia();
+        }
+    }
+
+    public void beginTimer() {
+        timer = new Timer();
+        task = new TimerTask() {
+            public void run() {
+                running = true;
+                double current = mediaPlayer.getCurrentTime().toSeconds();
+                double end = media.getDuration().toSeconds();
+                songProgressBar.setProgress(current / end);
+                if (current / end == 1) {
+                    cancelTimer();
+                }
+            }
+        };
+        timer.scheduleAtFixedRate(task, 1000, 1000);
+    }
+
+    public void cancelTimer() {
+        running = false;
+        timer.cancel();
+    }
+}
